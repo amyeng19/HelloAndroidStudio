d6cb8f5b67bdb5982c4f743db71649b4830e8fb9
amyeng19 <amyengineer19@gmail.com>
2020-10-30 00:11:00 -0400

    //Added h1 tag

---
+++ HelloAndroidStudio/app/src/main/java/com/example/helloandroidstudio/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.helloandroidstudio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun main() {
+        printGreeting("Hello, Android World")
+    }
+    //First modification: This is a function
+
+
+    //Write a function that takes a string as an argument, and returns “Hello, string”
+    fun printGreeting(personName: String) {
+        println ("Hello, Android World")
+    }
+
+}
